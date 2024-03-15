2aeb2dd99d2d9ae09a725c3194f690719b77cfa9
Андрей Клюев <113175638+DaggerHashimot0@users.noreply.github.com>
2024-03-15 20:31:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection() {
+    const section = document.getElementById('section');
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function openLink() {
+    window.open("https://t.me/DaggerHashimot0", "_blank"); 
+}
